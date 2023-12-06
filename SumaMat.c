cafa6914941d509dadfd4c8d6bfbdf7f09914daa
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-12-06 03:37:19 -0500

    Declaracion de funciones para impresion de las matrices A y B

--- SumaMat.c
+++ SumaMat.c
@@ -2,43 +2,75 @@
 #include <stdlib.h>
 #include <time.h>
 
-int fA, cA, fB, cB, fR, cR;
-void aleatoriosA (int MatrizA[fA][cA]);
-void aleatoriosB (int MatrizB[fB][cB]);
-void SumaMatrices(int MatrizA[fA][cA], int MatrizB[fB][cB], int MatrizR[fR][cR]);
+void Matriz1(int fA, int cA, int MatrizA[fA][cA]);
+void Matriz2(int fB, int cB, int MatrizB[fB][cB]);
+/* void SumaMatrices(int MatrizA[fA][cA], int MatrizB[fB][cB], int MatrizR[fR][cR]);*/
 
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
-
+    int fA, cA, fB, cB, fR, cR;
     printf("Dimensiones de la MATRIZ A\n");
     printf("FILAS: ");
     scanf("%d", &fA);
     printf("COLUMNAS: ");
     scanf("%d", &cA);
-    int MAtrizA[fA][cA];
+    int MatrizA[fA][cA];
+
+    printf("MATRIZ A: \n ");
+    for (int i = 0; i < fA; i++)
+    {
+        for (int j = 0; j < cA; j++)
+        {
+            MatrizA[i][j] = rand() % 101;
+        }
+    }
 
     printf("Dimensiones de la MATRIZ B\n");
     printf("FILAS: ");
     scanf("%d", &fB);
     printf("COLUMNAS: ");
     scanf("%d", &cB);
-    int MAtrizB[fB][cB];
+    int MatrizB[fB][cB];
 
-    printf("MATRIZ A: \n ");
-    aleatoriosA (MatrizA[fA][cA]);
+    printf("MATRIZ B: \n ");
+    for (int i = 0; i < fB; i++)
+    {
+        for (int j = 0; j < cB; j++)
+        {
+            MatrizB[i][j] = rand() % 101;
+        }
+    }
+
+    printf("MATRIZ A: \n");
+    Matriz1(fA, cA, MatrizA[fA][cA]);
+    printf("\n");
+    printf("MATRIZ B: \n");
+    Matriz2(fB, cB, MatrizB[fB][cB]);
 
     return 0;
 }
 
-    void aleatoriosA (int MatrizA[fA][cA]){
-        for (int i = 0; i < fA; i++)
+void Matriz1(int fA, int cA, int MatrizA[fA][cA])
+{
+    for (int i = 0; i < fA; i++)
+    {
+        for (int j = 0; j < cA; j++)
         {
-            for (int j = 0; j < cA; j++)
-            {
-                MatrizA[i][j]= rand % 101;
-            }
-            
+            printf("%d\t", MatrizA[i][j]);
         }
-        
+        printf("\n");
     }
+}
+
+void Matriz2(int fB, int cB, int MatrizB[fB][cB])
+{
+    for (int i = 0; i < fB; i++)
+    {
+        for (int j = 0; j < cB; j++)
+        {
+            printf("%d\t", MatrizB[i][j]);
+        }
+        printf("\n");
+    }
+}
