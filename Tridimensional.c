4ca42c33390d064a92da284d1fab68b772df6b5c
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-12-06 04:32:54 -0500

    Lineas de codigo para el ingreso de la dimension por teclado e Inicialización del arreglo en ceros

--- Tridimensional.c
+++ Tridimensional.c
@@ -11,6 +11,26 @@ int main(int argc, char const *argv[])
     printf("Ingrese la dimension 3: ");
     scanf("%d", &z);
     int Matriz[x][y][z];
-    
+
+    for (int k = 0; k < x; k++)
+    {
+        for (int i = 0; i < y; i++)
+        {
+            for (int j = 0; j < z; j++)
+            {
+                Matriz[k][i][j] = 0;
+            }
+        }
+    }
+
+    for (int i = 0; i < y; i++)
+    {
+        for (int j = 0; j < z; j++)
+        {
+
+            Matriz[x - 1][i][j] = 1;
+        }
+    }
+
     return 0;
 }
