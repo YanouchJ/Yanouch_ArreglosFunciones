d2c730d116bbbf416400bfdb3aa91827801fa59a
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-12-06 02:46:23 -0500

    Lineas de codigo para ingresar las dimensiones de las matrices A y B por teclado

--- SumaMat.c
+++ SumaMat.c
@@ -6,6 +6,21 @@ int main(int argc, char const *argv[])
 {
     srand(time(NULL));
     int fA, cA, fB, cB, fC, cC;
-    
+
+    printf("Dimensiones de la MATRIZ A\n");
+    printf("FILAS: ");
+    scanf("%d", &fA);
+    printf("COLUMNAS: ");
+    scanf("%d", &cA);
+    int MAtrizA[fA][cA];
+
+
+    printf("Dimensiones de la MATRIZ B\n");
+    printf("FILAS: ");
+    scanf("%d", &fB);
+    printf("COLUMNAS: ");
+    scanf("%d", &cB);
+    int MAtrizB[fB][cB];
+
     return 0;
 }
