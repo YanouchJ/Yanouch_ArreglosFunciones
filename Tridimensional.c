fc888353df71faad92715b017eb690b8a35aaf66
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-12-06 11:44:46 -0500

    Funcion para la impresion del arreglo

--- Tridimensional.c
+++ Tridimensional.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+int impresion(int *x, int y, int z, int Matriz(*)[y][z], int cont);
+
+return Matriz[x][y][z];
 
 int main(int argc, char const *argv[])
 {
-    int x, y, z, cont=1;
+    int x, y, z;
 
     printf("Ingrese la dimension 1: ");
     scanf("%d", &x);
@@ -32,5 +35,23 @@ int main(int argc, char const *argv[])
         }
     }
 
+    impresion(x, y, z, Matriz[x][y][z], cont);
     return 0;
+}
+
+int impresion(int x, int y, int z, int Matriz[x][y][z], int cont)
+{
+    for (int k = 0; k < x; k++)
+    {
+        //printf("Matriz %d:\n", cont++);
+        for (int i = 0; i < y; i++)
+        {
+            for (int j = 0; j < z; j++)
+            {
+                printf("%d\t", Matriz[k][i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
 }
