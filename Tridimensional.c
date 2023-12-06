a43fb06bc4e6a94632e870561f76601e86898668
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-12-06 11:54:35 -0500

    Ingreso de comentarios y fin del programa

--- Tridimensional.c
+++ Tridimensional.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+// declaracion de la funcion
 void impresion(int x, int y, int z, int Matriz[x][y][z]);
 
 int main(int argc, char const *argv[])
 {
     int x, y, z;
-
+    // Dimensiones de la matriz
     printf("Ingrese la dimension 1: ");
     scanf("%d", &x);
     printf("Ingrese la dimension 2: ");
@@ -13,6 +15,7 @@ int main(int argc, char const *argv[])
     scanf("%d", &z);
     int Matriz[x][y][z];
 
+    // Matriz inicializada en ceros
     for (int k = 0; k < x; k++)
     {
         for (int i = 0; i < y; i++)
@@ -23,7 +26,7 @@ int main(int argc, char const *argv[])
             }
         }
     }
-
+    // Ultima matriz inicializada en unos
     for (int i = 0; i < y; i++)
     {
         for (int j = 0; j < z; j++)
@@ -32,12 +35,13 @@ int main(int argc, char const *argv[])
             Matriz[x - 1][i][j] = 1;
         }
     }
+    // Impresion del arreglo
     printf("\nImpresion:  \n");
     impresion(x, y, z, Matriz[x][y][z]);
 
     return 0;
 }
-
+// Funcion para la impresion del arreglo
 void impresion(int x, int y, int z, int Matriz[x][y][z])
 {
     for (int k = 0; k < x; k++)
