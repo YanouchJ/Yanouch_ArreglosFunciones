4704b88513f3d40254a63c5a25062b79b7e9b815
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-12-06 04:30:19 -0500

    Inicio del programa e inicialización de variables de la Matriz

---
+++ Tridimensional.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int x, y, z, cont=1;
+
+    printf("Ingrese la dimension 1: ");
+    scanf("%d", &x);
+    printf("Ingrese la dimension 2: ");
+    scanf("%d", &y);
+    printf("Ingrese la dimension 3: ");
+    scanf("%d", &z);
+    int Matriz[x][y][z];
+    
+    return 0;
+}
