703ff954d703dd9082cfd53b38509f1bee1fd686
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-12-06 02:41:09 -0500

    Inicio del programa y declaración de primeras variables

---
+++ SumaMat.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char const *argv[])
+{
+    srand(time(NULL));
+    int fA, cA, fB, cB, fC, cC;
+    
+    return 0;
+}
